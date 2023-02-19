30a04756fe5a7e479593ede9339846e4f95354cb
DiogoicdSantos <diogoicdsantos@gmail.com>
2023-02-19 23:01:24 +0000

    refactor icons and grids

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import styled from "@emotion/styled";
 import Head from "next/head";
 import Image from "next/image";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin, AiFillRobot } from "react-icons/ai";
 import { CiMedal } from "react-icons/ci";
+import { DiVisualstudio } from "react-icons/di";
+import { FaJava, FaReact, FaUnity } from "react-icons/fa";
 import { GrLanguage } from "react-icons/gr";
 import pfp from "../../public/pfp.jpg";
 
@@ -15,9 +17,7 @@ const Main = styled.main`
 const NavBar = styled.nav`
   padding-top: 10px;
   padding-bottom: 5%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  padding-left: 5%;
 `;
 
 const Resume = styled.a`
@@ -44,6 +44,7 @@ const SayHello = styled.a`
 const Info = styled.div`
   display: flex;
   justify-content: space-evenly;
+  padding-bottom: 10rem;
   h2 {
     font-size: 3rem;
     color: #525252;
@@ -65,8 +66,8 @@ const NavBarButtons = styled.div`
 `;
 
 const MyIcons = styled.div`
-  padding-top: 5rem;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   font-size: 3vh;
   gap: 15px;
@@ -87,14 +88,14 @@ const AboutMeTitles = styled.div`
   align-items: center;
 `;
 
-const AboutMeBox = styled.div`
+const ContentBox = styled.div`
   gap: 1.5rem;
   padding: 1em;
   border: 1px solid #e6e6e6;
   border-radius: 5%;
-  display: grid;
-  justify-items: center;
-  grid-template-columns: 1fr;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const AboutMeInfo = styled.div`
@@ -117,19 +118,52 @@ const CustomGrLanguage = styled(GrLanguage)`
   width: 3em;
 `;
 
+const CustomFaUnity = styled(FaUnity)`
+  height: 3em;
+  width: 3em;
+`;
+
+const CustomFaReact = styled(FaReact)`
+  height: 3em;
+  width: 3em;
+`;
+
+const CustomFaJava = styled(FaJava)`
+  height: 3em;
+  width: 3em;
+`;
+
+const CustomDiVisualstudio = styled(DiVisualstudio)`
+  height: 3em;
+  width: 3em;
+`;
+
+const CustomAiFillRobot = styled(AiFillRobot)`
+  height: 3em;
+  width: 3em;
+`;
+
+const ToolsTitles = styled.div`
+  text-align: center;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+
 const ToolsDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  p {
-    max-width: 75ch;
-  }
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
 `;
 
 const PFP = styled(Image)`
-  height: 400px;
+  height: 25rem;
   width: auto;
   border-radius: 5%;
+`;
+
+const BodySection = styled.section`
+  margin-left: 10%;
+  margin-right: 10%;
 `;
 
 export default function Home() {
@@ -142,16 +176,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main>
-        <section>
-          <NavBar>
-            <h1>Diogo</h1>
-            <NavBarButtons>
-              <Resume href="https://www.linkedin.com/in/diogo-santos-02336b177/">
-                Resume
-              </Resume>
-            </NavBarButtons>
-          </NavBar>
+        <NavBar>
+          <h1>Diogo</h1>
+        </NavBar>
+        <BodySection>
           <Info>
+            <MyIcons>
+              <a href="https://www.linkedin.com/in/diogo-santos-02336b177/">
+                <AiFillLinkedin />
+              </a>
+              <a href="https://github.com/DiogoicdSantos">
+                <AiFillGithub />
+              </a>
+            </MyIcons>
             <InfoText>
               <h2>Diogo Santos </h2>
               <h3>Software Developer.</h3>
@@ -163,8 +200,6 @@ export default function Home() {
             </InfoText>
             <PFP alt="pfp" src={pfp}></PFP>
           </Info>
-        </section>
-        <section>
           <AboutMeTitles>
             <h3>About me</h3>
             <h2>Introduction</h2>
@@ -174,21 +209,21 @@ export default function Home() {
               <PFP alt="pfp" src={pfp}></PFP>
             </div>
             <AboutMeInfo>
-              <AboutMeBox>
+              <ContentBox>
                 <CustomCiMedal />
                 <div>Experience</div>
                 <div>2.5 years</div>
-              </AboutMeBox>
-              <AboutMeBox>
+              </ContentBox>
+              <ContentBox>
                 <CustomGrLanguage />
                 <div>English</div>
                 <div>10/10</div>
-              </AboutMeBox>
-              <AboutMeBox>
+              </ContentBox>
+              <ContentBox>
                 <CustomGrLanguage />
                 <div>Portuguese</div>
                 <div>10/10</div>
-              </AboutMeBox>
+              </ContentBox>
               <p className="grid-col-span-3">
                 Since the beginning of my carrer as a developer, I've worked
                 remotely for companies, collaborating with great people to
@@ -199,20 +234,38 @@ export default function Home() {
               </Resume>
             </AboutMeInfo>
           </AboutMe>
-
-          <MyIcons>
-            <AiFillLinkedin href="https://www.linkedin.com/in/diogo-santos-02336b177/" />
-            <AiFillGithub href="https://github.com/DiogoicdSantos" />
-          </MyIcons>
-          <ToolsDiv>
+          <ToolsTitles>
             <h3>Great Software</h3>
             <h3>Tools I've used</h3>
-            <p>Reactjs</p>
-            <p>Nodejs</p>
-            <p>Unity</p>
-            <p>Chat bots</p>
+          </ToolsTitles>
+          <ToolsDiv>
+            <ContentBox>
+              <CustomFaReact />
+              <div>Reactjs</div>
+              <div>6/10</div>
+            </ContentBox>
+            <ContentBox>
+              <CustomFaJava />
+              <div>JavaScript</div>
+              <div>6/10</div>
+            </ContentBox>
+            <ContentBox>
+              <CustomDiVisualstudio />
+              <div>C#</div>
+              <div>4/10</div>
+            </ContentBox>
+            <ContentBox>
+              <CustomFaUnity />
+              <div>Unity</div>
+              <div>4/10</div>
+            </ContentBox>
+            <ContentBox>
+              <CustomAiFillRobot />
+              <div>Chat bots</div>
+              <div>10/10</div>
+            </ContentBox>
           </ToolsDiv>
-        </section>
+        </BodySection>
       </Main>
     </>
   );
